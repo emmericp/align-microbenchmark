3a1ff215730c5f9d296962d03288068b555c500e
Maximilian Pudelko <maximilian.pudelko@tum.de>
2017-03-02 01:10:45 +0100

    Adds generic align function.
    Benchmarks indicate that we mostly benchamrked the align function, not the actual effects of alignment

--- benchmark.lua
+++ benchmark.lua
@@ -19,15 +19,20 @@ function configure(parser)
 	parser:option("-f --filter", "pcap filter to use."):default("host 10.0.0.0")
 	parser:option("--fixed-size", "Use a fixed packet size instead of a realistic distribution."):target("fixedSize"):convert(tonumber)
 	parser:option("-r --runs", "Repeat the test n times."):default("1"):convert(tonumber)
+	parser:option("--alignment", "Align to Nth byte."):default("1"):convert(tonumber)
+	parser:flag("--null-filter", "Overwrites filter with catchall."):target("NullFilter")
 	parser:mutex(
 		parser:flag("--packed", "No alignment"),
 		parser:flag("--l2-aligned", "Align layer 2 header on 4-byte boundaries."):target("l2Aligned"),
 		parser:flag("--l3-aligned", "Align layer 3 header on 4-byte boundaries."):target("l3Aligned"),
-		parser:flag("--even-aligned", "Align at 2-byte boundaries."):target("evenAligned")
+		parser:flag("--even-aligned", "Align at 2-byte boundaries."):target("evenAligned"),
+		parser:flag("--b8-aligned", "Align at 8-byte boundaries."):target("B8Aligned"),
+		parser:flag("--n-aligned", "Align at N-byte boundaries."):target("NAligned")
+		
 	)
 	args = parser:parse()
 	-- TODO: argparse should be able to enforce this somehow
-	if not args.packed and not args.l2Aligned and not args.l3Aligned and not args.evenAligned then
+	if not args.packed and not args.l2Aligned and not args.l3Aligned and not args.evenAligned and not args.B8Aligned and not args.NAligned then
 		parser:error("Specifiy either --packed, --l2-aligned, or --l3-aligned")
 	end
 	return args
@@ -73,6 +78,16 @@ local function alignL2(num)
 	-- packet data starts add +10 bytes
 	return alignL3(num + 2) - 2
 end
+local function align8(num)
+	-- return (num + 7) & ~7
+	return bit.band(num + 7ULL, bit.bnot(7ULL))
+end
+local function align(num, boundary, offset)
+	offset = offset or 0
+	boundary = boundary or 1
+	-- return (num + boundary - 1) & ~(boundary - 1)
+	return bit.band(num + boundary - 1ULL, bit.bnot(boundary - 1ULL))
+end
 
 local voidPtr = ffi.typeof("void*")
 
@@ -80,6 +95,7 @@ local function runTest(mem, memSize, numPkts, align, filter)
 	local i = 0
 	local match, discard = 0, 0
 	local start = getMonotonicTime() -- can't use libmoon.getTime() without initializing DPDK
+	local data = 0
 	for pkt = 1, numPkts do
 		i = align(i)
 		local pkt = packetType(voidPtr(mem + i))
@@ -90,11 +106,13 @@ local function runTest(mem, memSize, numPkts, align, filter)
 			discard = discard + 1
 		end
 		i = i + size + headerSize
+		--data = data + size
 	end
 	local stop = getMonotonicTime()
 	local time = stop - start
 	local pktRate = (match + discard) / time
 	local dataRate = memSize * 8 / time
+	--local dataRate = data * 8 / time
 	log:info("Filtered %d packets, accepted %.2f%%.", match + discard, match / (match + discard) * 100)
 	log:info("Time elapsed: %.2f milliseconds", time * 1000)
 	log:info("%.2f Mpps, %.2f Gbit/s", pktRate / 10^6, dataRate / 10^9)
@@ -103,6 +121,7 @@ end
 
 local function randomSize()
 	return math.random(60, 124)
+	--return math.random(60, 92)
 end
 
 function master(args)
@@ -111,13 +130,18 @@ function master(args)
 		or args.l2Aligned and alignL2
 		or args.l3Aligned and alignL3
 		or args.evenAligned and alignEven
+		or args.B8Aligned and align8
+		or args.NAligned and function(num) return align(num, args.alignment) end
 		or error("no alignment specified")
 	local memSize = args.memory * 2^30
 	local mem = memory.allocHuge("uint8_t*", memSize)
 	local template, templatePkt = makeTemplate()
 	local i = 0
 	local numPkts = 0
-	log:info("Header size: " .. headerSize)
+	log:info("Header size: %d", headerSize)
+	for i,v in pairs({0,1,2,3,4,7,8,9,15,16}) do
+		log:info("align(%d) = %d", v, tonumber(align(v)))
+	end
 	log:info("Generating packets in memory.")
 	while true do
 		local size = args.fixedSize or randomSize()
@@ -139,9 +163,14 @@ function master(args)
 	end
 	local times, pktRates, dataRates = {}, {}, {}
 	--(require"jit.dump").on()
+	local filter_fn = pf.compile_filter(args.filter)
+	if args.NullFilter then
+		log:info("Using null filter")
+		filter_fn = function(data, size) return false end
+	end
 	for i = 1, args.runs do
 		log:info("Running test run %d/%d", i, args.runs)
-		local time, pktRate, dataRate = runTest(mem, memSize, numPkts, align, pf.compile_filter(args.filter))
+		local time, pktRate, dataRate = runTest(mem, memSize, numPkts, align, filter_fn)
 		times[#times + 1] = time * 1000 -- ms
 		pktRates[#pktRates + 1] = pktRate / 10^6 -- mpps
 		dataRates[#dataRates + 1] = dataRate / 10^9 -- gbit

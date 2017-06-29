6ccb98d62dd8b299fcce0ba6d543ca8dd6ab4820
Maximilian Pudelko <maximilian.pudelko@tum.de>
2017-06-29 20:07:18 +0200

    Add all-in-one script to run benchmarks

--- formatResults.py
+++ formatResults.py
@@ -10,11 +10,11 @@ with open(sys.argv[1], newline='', encoding='utf-8') as csvfile:
     if first[0].startswith('#'):
         data.append(first)
         first = next(reader)
-    reference = int(first[1])
+    reference = float(first[1])
     data.append([first[0], 100])
     for row in reader:
-        data.append([row[0], int(row[1])/reference])
+        data.append([row[0], float(row[1])/reference])
 
-with open(sys.argv[1] + "out.csv", 'w', newline='') as f:
+with open(sys.argv[1].split(".")[0] + "_formated.csv", 'w', newline='') as f:
     writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
     writer.writerows(data)
---
+++ run_benchmark.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+trap exit SIGINT
+
+CPU=$1
+size=$2
+declare -a vary=("ethertype" "ip" "ip+port")
+declare -a filter=("ether proto ip6" "dst host 0.0.0.0" "dst host 0.0.0.0 udp port 0")
+
+for i in ${!vary[@]}
+do
+	for traffic in uniform lrz
+	do
+		echo "# ${CPU} ${traffic} ${vary[$i]}" >> "benchmark_${CPU}_${traffic}_${vary[$i]}.csv"
+		for align in {1,2,4,8,64}
+		do
+			./libmoon/build/libmoon benchmark.lua -r 100 -m $size -f "${filter[$i]}" --n-aligned --alignment $align --offset 0 --traffic=$traffic --vary="${vary[$i]}" --robot >> "benchmark_${CPU}_${traffic}_${vary[$i]}.csv"
+		done
+		./libmoon/build/libmoon benchmark.lua -r 100 -m $size -f "${filter[$i]}" --n-aligned --alignment 4 --offset 2 --traffic=$traffic --vary="${vary[$i]}" --robot >> "benchmark_${CPU}_${traffic}_${vary[$i]}.csv"
+		./libmoon/build/libmoon benchmark.lua -r 100 -m $size -f "${filter[$i]}" --n-aligned --alignment 8 --offset 2 --traffic=$traffic --vary="${vary[$i]}" --robot >> "benchmark_${CPU}_${traffic}_${vary[$i]}.csv"
+		./formatResults.py "benchmark_${CPU}_${traffic}_${vary[$i]}.csv"
+	done
+done
